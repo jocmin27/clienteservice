a5d94f674098970d63ca676e26b05bc2397958f5
chodmin <ayrtonjor@hotmail.com>
2024-07-09 16:51:39 -0500

    update clientService

--- src/test/java/com/example/clientservice/ClienteServiceIntegrationTest.java
+++ src/test/java/com/example/clientservice/ClienteServiceIntegrationTest.java
@@ -3,40 +3,40 @@ package com.example.clientservice;
 import com.example.clientservice.entity.Client;
 import com.example.clientservice.repository.ClientRepository;
 import com.example.clientservice.service.ClientService;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
-public class ClientServiceTests {
+@Transactional
+public class ClienteServiceIntegrationTest {
 
-    @InjectMocks
+    @Autowired
     private ClientService clienteService;
 
-    @Mock
+    @Autowired
     private ClientRepository clienteRepository;
 
     private Client cliente;
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this);
         cliente = new Client();
-        cliente.setId(1L);
         cliente.setNombre("John Doe");
     }
 
     @Test
-    void testCreateCliente() {
-        when(clienteRepository.save(cliente)).thenReturn(cliente);
+    void testCreateAndGetCliente() {
         Client createdCliente = clienteService.saveCliente(cliente);
         assertNotNull(createdCliente);
+        assertNotNull(createdCliente.getId());
+
+        Client foundCliente = clienteService.getClienteById(createdCliente.getId());
+        assertNotNull(foundCliente);
+        assertEquals("John Doe", foundCliente.getNombre());
     }
 }
-
